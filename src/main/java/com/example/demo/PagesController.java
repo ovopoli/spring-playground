1a3917aef5742e62a5baa9aec231a74b17d8225b
Poli Gonzalez <gonzalezpoli10@gmail.com>
2020-12-08 15:21:27 -0600

    Calculate endpoints

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +16,13 @@ public class PagesController {
     @GetMapping("/math/pi")
     public String pi() {
         return "3.141592653589793";
+    }
+    @GetMapping("/math/calculate")
+    public String calculate(@RequestParam Integer x, @RequestParam Integer y, @RequestParam(defaultValue = "add") String operation) {
+        return MathService.calculate(operation, x, y).toString();
+    }
+    @PostMapping("/math/sum")
+    public String sum(@RequestParam Integer [] n) {
+        return MathService.sum(n).toString();
     }
 }
--- src/test/java/com/example/demo/PagesControllerTest.java
+++ src/test/java/com/example/demo/PagesControllerTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,4 +33,41 @@ public class PagesControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("3.141592653589793"));
     }
+    @Test
+    public void testingPostSum() throws Exception {
+        this.mvc.perform(post("/math/sum?n=4&n=5&n=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("15"));
+    }
+    @Test
+    public void testCalculateNoOperation() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=4&y=6"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+    @Test
+    public void testCalculateAdd() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=4&y=6&operation=add"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
+    }
+    @Test
+    public void testCalculateSubtract() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=4&y=6&operation=subtract"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("-2"));
+    }
+    @Test
+    public void testCalculateMultiply() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=4&y=6&operation=multiply"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("24"));
+    }
+    @Test
+    public void testCalculateDivide() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=30&y=5&operation=divide"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("6"));
+    }
+
 }
