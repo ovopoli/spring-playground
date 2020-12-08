5d49d10a8b13a174ba37104f8423089cdae511b5
Poli Gonzalez <gonzalezpoli10@gmail.com>
2020-12-08 13:14:44 -0600

    /hello endpoint

---
+++ src/test/java/com/example/demo/PagesControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PagesController.class)
+public class PagesControllerTest {
+    @Autowired
+    MockMvc mvc;
+    
+    @Test
+    public void testingPages() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/hello");
+        
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+}
